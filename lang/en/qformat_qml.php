32562df52d876bbe51821488c0f0356e18724671
Tom McCracken <tommccracken@btinternet.com>
2015-03-05 16:24:22 +0000

    Added help strings

--- lang/en/qformat_qml.php
+++ lang/en/qformat_qml.php
@@ -1,5 +1,6 @@
 <?php
 
 $string['pluginname'] = 'Questionmark QML Importer';
+$string['pluginname_help'] = 'Imports supported Questionmark QML questions and converts them into the moodle equivalent.';
 $string['unknownquestiontype'] = 'Question type {$a} is not supported by QML import';
 $string['blankmultiquestionhint'] = "   (Separate answers with a comma and no spaces e.g. answer1,answer2,answer3...)";
