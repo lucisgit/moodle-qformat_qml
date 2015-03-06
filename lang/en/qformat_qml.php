65e6d8e635ed30c58725272ceea452b3528c4633
Tom McCracken <tommccracken@btinternet.com>
2015-03-06 14:25:48 +0000

    Updated file header information and added a version file.

--- lang/en/qformat_qml.php
+++ lang/en/qformat_qml.php
@@ -1,4 +1,26 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qformat_qml', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package    qformat_qml
+ * @copyright  2015, Lancaster University ISS
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 $string['pluginname'] = 'Questionmark QML Importer';
 $string['pluginname_help'] = 'Imports supported Questionmark QML questions and converts them into the moodle equivalent.';
